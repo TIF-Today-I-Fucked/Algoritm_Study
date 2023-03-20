56d77f247f2919302569619b50680b8e2b5d6af2
ulol_wsl <jeffk0408@naver.com>
2023-03-20 11:40:02 +0900

    feat : first dkim2 fail

---
+++ dkim2_test/[0320_1629].cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+unsigned long modddd( int a, int b, int c );
+
+int main()
+{
+    int a, b, c;
+
+    std::cin >> a >> b >> c;
+
+    
+    std::cout << res << std::endl;
+}
+
+unsigned long modddd( int a, int b, int c ) {
+    
+}
