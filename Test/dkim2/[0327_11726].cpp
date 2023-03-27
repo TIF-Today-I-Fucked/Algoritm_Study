68781f81b3e0478768fdb95992ef6af728fc188d
ulol_wsl <jeffk0408@naver.com>
2023-03-27 10:52:10 +0900

    test dkim2

---
+++ Test/dkim2/[0327_11726].cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int count;
+
+    std::cin >> count;
+
+    if (count == 1) {
+        std::cout <<  "1" << std::endl;
+        return 0;
+    }
+    if (count == 2)
+    {
+        std::cout <<  "2" << std::endl;
+        return 0;
+    }
+    if (count == 3)
+    {
+        std::cout <<  "3" << std::endl;
+        return 0;
+    }
+    
+    std::vector<int> ways(count);
+    ways[0] = 1;
+    ways[1] = 2;
+
+
+    for (int i = 2; i < count ; i++ )
+    {
+        ways[i] = (ways[i - 1] + ways[i - 2]) % 10007;
+    }
+    std::cout <<  ways[count - 1] << std::endl;
+    return 0;
+}
