6e4d734c68f6e698e2a91e18bf52a1b39c485311
change-challenge <dog941128@hotmail.com>
2023-04-18 11:19:26 +0900

    0418 - 2667 apartment

--- Algorithms/DFS_BFS/[Baekjoon]_2667/2667_hchang.cpp
+++ Algorithms/DFS_BFS/[Baekjoon]_2667/2667_hchang.cpp
@@ -7,37 +7,55 @@
 #include <algorithm>
 #include <vector>
 
-std::vector<std::string > arr;
-std::vector<std::vector<bool> > isVisted;
-std::vector<int> res;
+#define MAX 25
 
-void	dfs(int num)
+std::vector<std::string>	map;
+bool						visited[MAX][MAX];
+int							num;
+int							m_x[4] = {1, -1, 0, 0};
+int							m_y[4] = {0, 0, 1, -1};
+
+int	dfs(int x, int y, int& res)
 {
+	if (x < 0 || x >= num || y < 0 || y >= num || visited[y][x] == true || map[y][x] == '0')
+		return res;
+	res++;
+	visited[y][x] = true;
+	for (int i = 0 ; i < 4; i++)
+		dfs(m_x[i] + x, m_y[i] + y, res);
+	return res;
 
 }
 
 int main()
 {
-	int num;
+	std::vector<int>	res;
+	int					tmp_res(0);
 	std::cin >> num;
 
 	for (int i = 0; i < num; i++)
 	{
 		std::string tmp;
 		std::cin >> tmp;
-		arr.push_back(tmp);
+		map.push_back(tmp);
 	}
 
 	for (int i = 0; i < num; i++)
 	{
-		for (int i = 0; i < num; i++)
-
+		for (int j = 0; j < num; j++)
+		{
+			if (map[i][j] == '1' && visited[i][j] == false)
+			{
+				res.push_back(dfs(j, i, tmp_res));
+				tmp_res = 0;
+			}
+		}
 	}
 
-	dfs(num);
-
-	std::cout << res.size();
 	std::sort(res.begin(), res.end());
-	for (int i = 0 ; i < res.size(); i++)
+	std::cout << res.size() << std::endl;
+	for (int i = 0; i < res.size(); i++)
+	{
 		std::cout << res[i] << std::endl;
+	}
 }
