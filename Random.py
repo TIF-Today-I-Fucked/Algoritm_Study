ee8648a77901dd3c5ff48b595e6614f7cdf345a6
change-challenge <dog941128@hotmail.com>
2023-04-28 12:31:19 +0900

    0428

--- Random.py
+++ Random.py
@@ -1,10 +1,17 @@
 import os
 import random
 
-os.chdir("./Algorithms/DynamicProgramming")
+os.chdir("./Algorithms/DFS_BFS")
 lst = os.listdir()
-new_list = [string.replace("[Baekjoon]_", "") for string in lst]
+new_list1 = [string.replace("[Baekjoon]_", "") for string in lst]
+
+os.chdir("../BackTracking")
+lst = os.listdir()
+new_list2 = [string.replace("[Baekjoon]_", "") for string in lst]
+
+new_list = new_list1 + new_list2
 random_items = random.sample(new_list, k=2)
 
 print(" 🔥 오늘의 문제는 ?!")
 print("	첫 번째, 두 번째 문제 ->", random_items ," 입니다!")
+
